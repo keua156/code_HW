ce7c6795a2cffbcba0940c17076c10997734b064
keua156 <117151431+keua156@users.noreply.github.com>
2023-12-13 22:49:18 +0700

    Add files via upload

--- lab1.1.cpp
+++ lab1.1.cpp
@@ -1,35 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-
-void GetSet( int array[], int *N ) ;
-
-int main() 
+int main () 
 {
+	int n = 0;
+do {
 	
-   int data[20],num;
-   GetSet(data, &num );
-   
-    printf("|--- Mamber ---|\n");
-    for (int i = 0; i < num; i++) {
-        printf("Member[%d] : %d \n",i+1,data[i]);
-    }
-    printf("|--- ALL Mamber Input : %d ---|", num);
- 
- return 0 ;
- 	
-}//end function
-
-void GetSet( int array[], int *N ) {
-	int i;
+	printf("INPUT : ");
+	scanf("%d",&n);
 	
-	 printf("Number Member : ");
- 	 scanf("%d",N);
- 	 
- 	 printf("INPUT Data :\n");
-    for (i = 0; i < *N; i++) {
-        printf("Member %d : ", i+1);
-        scanf("%d", &array[i]);
-    }
-
- 
+	
+	for(int i = 1 ; i <= n ; i++){
+		printf("[ %d ] hello world \n",i);
+		}
+		
+	}while(true);
 }
