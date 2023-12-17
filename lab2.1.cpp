470aa921017642404506bf512235a23d94194834
keua156 <117151431+keua156@users.noreply.github.com>
2023-12-17 15:50:51 +0700

    Add files via upload

--- lab2.1.cpp
+++ lab2.1.cpp
@@ -3,25 +3,20 @@
 void reverse(char str1[50], char str2[50]);
 
 int main() {
-    char text[50] = "I Love You";
+    char text[50] = "Hello World";
     char out[50];
     reverse(text, out);
-    printf("Original: %s\nReversed: %s\n", text, out);
+    printf("--|Input: %s|--\n--|output: %s|--\n", text, out);
     return 0;
 }
 
 void reverse(char str1[50], char str2[50]) {
-    // Find the length of the string
     int length = 0;
     while (str1[length] != '\0') {
         length++;
-    }
-
-    // Copy characters in reverse order to str2
+    } 
     for (int i = 0; i < length; i++) {
         str2[i] = str1[length - 1 - i];
     }
-
-    // Add null terminator to the end of str2
-    str2[length] = '\0';
+	str2[length] = '\0';
 }
